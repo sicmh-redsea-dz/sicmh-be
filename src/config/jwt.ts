1a4711bff862e44d423216130ddda71ec89070e9
Dereck Zavala <zavaladereck77@gmail.com>
2025-09-23 21:15:12 -0600

    changes for build.

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -1,9 +1,6 @@
 import { config } from './env'
 import jwt from 'jsonwebtoken'
 
-export const generateToken = (payload: object) => {
-    return jwt.sign(payload, config.SECRET_JWT_TOKEN, { expiresIn: config.JWT_EXPIRES_IN })
-}
 
 export const verifyToken = (token: string) => {
     return jwt.verify(token, config.SECRET_JWT_TOKEN)
